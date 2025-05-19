028fd0ec667308a44743ce29578b52dde04bb9bc
gaming73152 <elden.hui@gmail.com>
2025-05-19 13:17:57 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -9,7 +9,26 @@ gui = {}
 function gui:test(x) -- to test
   print("test yay",x)
 end
+--[[ Settings:
 
-gui.test2 = "hi"
+({
+Name = "Cool Hub",
+LoadingTitle = "Cool Hub",
+LoadingSubTitle = "by theyesaok",
+Configuration = {
+  Enabled = true,
+  File = "CoolHub",
+},
+
+
+})
+
+
+]]
+function gui:init(settings) -- initilaze the gui
+  local initfunctions = {}
+  
+  return initfunctions
+end
 
 return gui
